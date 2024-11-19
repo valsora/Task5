b664480d2c6f9e8aff14e73e8170b6743f20960a
valsora <valsora9@gmail.com>
2024-11-19 20:55:09 +0300

    finished

--- Task5.java
+++ Task5.java
@@ -1,6 +1,12 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
 public class Task5 {
     public static void main(String[] args) {
         System.out.println("1. " + sameLetterPattern("ABCBA", "BCDCB"));
@@ -8,11 +14,11 @@ public class Task5 {
         System.out.println("3. " + digitsCount(1289396387328L));
         System.out.println("4. " + totalPoints(new String[] {"trance", "recant"}, "recant"));
         System.out.println("5. " + longestRun(new int[] {1, 2, 3, 5, 6, 7, 8, 9}));
-        System.out.println(". ");
+        System.out.println("6. " + takeDownAverage(new String[] {"95%", "83%", "90%", "87%", "88%", "93%"}));
         System.out.println("7. " + canMove("Queen", "C4", "D6"));
         System.out.println("8. " + maxPossible(8732, 91255));
-        System.out.println(". ");
-        System.out.println(". ");
+        System.out.println("9. " + timeDifference("New York", "December 31, 1970 13:40", "Beijing"));
+        System.out.println("10. " + isNew(509));
     }
 
     public static boolean sameLetterPattern(String s1, String s2) {
@@ -99,6 +105,17 @@ public class Task5 {
         return Math.max(maxLengthInc, maxLengthDec);
     }
 
+    public static String takeDownAverage(String[] grades) {
+        if (grades.length == 0) return null;
+        double sum = 0;
+        for (String stringGrade : grades) {
+            sum += Double.parseDouble(stringGrade.substring(0, stringGrade.length() - 1));
+        }
+        double myGrade = (sum / grades.length - 5) * (grades.length + 1) - sum;
+        if (myGrade < 0) return null; 
+        return Math.round(myGrade) + "%";
+    }
+
     public static boolean canMove(String name, String from, String to) {
         int fromLetter = Character.getNumericValue(from.charAt(0)) - 64;
         int fromNumber = Character.getNumericValue(from.charAt(1));
@@ -141,5 +158,107 @@ public class Task5 {
             }
         }
         return Integer.parseInt(s);
+    }
+
+    public static String timeDifference(String cityA, String timestamp, String cityB) {
+        SimpleDateFormat formatterIn = new SimpleDateFormat("MMMM d, yyyy HH:mm", Locale.ENGLISH);
+        SimpleDateFormat formatterOut = new SimpleDateFormat("yyyy-M-d HH:mm", Locale.ENGLISH);
+        String tzIDA = "GMT";
+        String tzIDB = "GMT";
+        switch (cityA) {
+            case "Los Angeles":
+                tzIDA += "-0800";
+                break;
+            case "New York":
+                tzIDA += "-0500";
+                break;
+            case "Caracas":
+                tzIDA += "-0430";  
+                break;
+            case "Buenos Aires":
+                tzIDA += "-0300";
+                break;
+            case "London":
+                tzIDA += "+0000";  
+                break;
+            case "Rome":
+                tzIDA += "+0100";  
+                break;
+            case "Moscow":
+                tzIDA += "+0300";  
+                break;
+            case "Tehran":
+                tzIDA += "+0330";
+                break;
+            case "New Delhi":
+                tzIDA += "+0530";
+                break;
+            case "Beijing":
+                tzIDA += "+0800";
+                break;
+            case "Canberra":
+                tzIDA += "+1000";
+                break;
+            default:
+                tzIDA += "+0000";
+                break;
+        }
+        switch (cityB) {
+            case "Los Angeles":
+                tzIDB += "-0800";
+                break;
+            case "New York":
+                tzIDB += "-0500";
+                break;
+            case "Caracas":
+                tzIDB += "-0430";  
+                break;
+            case "Buenos Aires":
+                tzIDB += "-0300";
+                break;
+            case "London":
+                tzIDB += "+0000";  
+                break;
+            case "Rome":
+                tzIDB += "+0100";  
+                break;
+            case "Moscow":
+                tzIDB += "+0300";  
+                break;
+            case "Tehran":
+                tzIDB += "+0330";
+                break;
+            case "New Delhi":
+                tzIDB += "+0530";
+                break;
+            case "Beijing":
+                tzIDB += "+0800";
+                break;
+            case "Canberra":
+                tzIDB += "+1000";
+                break;
+            default:
+                tzIDB += "+0000";
+                break;
+        }
+        formatterIn.setTimeZone(TimeZone.getTimeZone(tzIDA));
+        formatterOut.setTimeZone(TimeZone.getTimeZone(tzIDB));
+        try {
+            Date date = formatterIn.parse(timestamp);
+            return formatterOut.format(date);
+        } catch (ParseException e) {
+            System.out.println(e.toString());
+        }
+        return null;
+    }
+
+    public static boolean isNew(int n) {
+        String s = Integer.toString(n);
+        int firstDigit = Character.getNumericValue(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            int digit = Character.getNumericValue(s.charAt(i));
+            if (digit < firstDigit && digit != 0) return false;
+        }
+        return true;
     }
 }
